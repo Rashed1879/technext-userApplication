a4990c604b758ccd53c600d4ae6dfae58ac55c8c
Rashed1879 <rashedul15-1879@diu.edu.bd>
2024-02-10 00:50:49 +0600

    sorting option added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import HeadLine from './components/HeadLine/HeadLine';
 function App() {
 	const [userData, setUserData] = useState([]);
 	const [searchTerm, setSearchTerm] = useState('');
+	const [sortOption, setSortOption] = useState('');
 
 	useEffect(() => {
 		fetch('https://dummyjson.com/users')
@@ -17,7 +18,24 @@ function App() {
 		setSearchTerm(event.target.value);
 	};
 
-	const filteredUsers = userData.filter(
+	const handleSort = (event) => {
+		setSortOption(event.target.value);
+	};
+
+	const sortedUsers = [...userData].sort((a, b) => {
+		switch (sortOption) {
+			case 'sortByName':
+				return a.firstName.localeCompare(b.firstName);
+			case 'sortByEmail':
+				return a.email.localeCompare(b.email);
+			case 'sortByCompanyName':
+				return a.company.name.localeCompare(b.company.name);
+			default:
+				return 0;
+		}
+	});
+
+	const filteredUsers = sortedUsers.filter(
 		(user) =>
 			user.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
 			user.lastName.toLowerCase().includes(searchTerm.toLowerCase())
@@ -28,18 +46,35 @@ function App() {
 			<div className="mt-5 container mx-auto px-5">
 				<HeadLine title="User Application" />
 				{/* for search bar and filter options */}
-				<div className="my-10">
-					<div className="relative flex items-center h-12 w-96 rounded-lg focus-within:shadow-lg overflow-hidden border border-gray-700">
+				<div className="my-10 flex flex-col md:flex-row justify-between items-center space-y-5 md:space-y-0">
+					<div className="relative flex items-center h-12 w-full md:w-64 lg:w-96 rounded-lg focus-within:shadow-lg overflow-hidden border border-gray-700">
 						<div className="flex justify-center items-center text-2xl w-12 h-full text-gray-400">
 							<GoSearch />
 						</div>
 						<input
-							className="peer h-full w-full outline-none text-lg text-gray-700 px-2"
+							className="h-full w-full outline-none text-lg text-gray-700 px-2"
 							type="text"
 							id="search"
 							placeholder="Search username.."
 							onChange={handleSearch}
 						/>
+					</div>
+					<div className="w-full md:w-64 lg:w-96">
+						<select
+							id="sortOptions"
+							className="border border-gray-700 text-sm text-black rounded-lg focus:border-gray-500 w-full h-12 p-2"
+							onChange={handleSort}
+							defaultValue=""
+						>
+							<option disabled value="">
+								Sort by
+							</option>
+							<option value="sortByName">Sort by name</option>
+							<option value="sortByEmail">Sort by email</option>
+							<option value="sortByCompanyName">
+								Sort by Company name
+							</option>
+						</select>
 					</div>
 				</div>
 
